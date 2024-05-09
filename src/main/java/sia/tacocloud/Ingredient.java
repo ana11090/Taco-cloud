affeb93492c4a68f7b971a788a2d175b9a70edee
ana11090 <72621705+ana11090@users.noreply.github.com>
2024-05-09 23:16:28 +0300

    in work

--- src/main/java/sia/tacocloud/Ingredient.java
+++ src/main/java/sia/tacocloud/Ingredient.java
@@ -1,16 +1,27 @@
 package sia.tacocloud;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA-compatible no-arg constructor
 public class Ingredient {
-    private final String id;
-    private final String name;
-    private final Type type;
+    @Id
+    private String id;
+    private String name;
 
-    public static enum Type {
+    @Enumerated(EnumType.STRING)  // To store enum values as strings
+    private Type type;
+
+    public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
 }
--- src/main/java/sia/tacocloud/JdbcIngredientRepository.java
+++ src/main/java/sia/tacocloud/JdbcIngredientRepository.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import sia.tacocloud.Ingredient;
@@ -25,14 +28,56 @@ public abstract class JdbcIngredientRepository implements IngredientRepository {
         return jdbc.query("select id, name, type from Ingredient",
                 this::mapRowToIngredient);
     }
-
     @Override
     public Ingredient findOne(String id) {
-        return jdbc.queryForObject("select id, name, type from Ingredient where id=?",
-                this::mapRowToIngredient, id);
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(
+                    "select id, name, type from Ingredient");
+            statement.setString(1, id);
+            resultSet = statement.executeQuery();
+            Ingredient ingredient = null;
+            if(resultSet.next()) {
+                ingredient = new Ingredient(
+                        resultSet.getString("id"),
+                        resultSet.getString("name"),
+                        Ingredient.Type.valueOf(resultSet.getString("type")));
+            }
+            return ingredient;
+        } catch (SQLException e) {
+            // ??? What should be done here ???
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {}
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {}
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {}
+            }
+        }
+        return null;
     }
 
-    private Ingredient mapRowToIngredient(ResultSet rs, int rowNum) throws SQLException {
+    private JdbcTemplate jdbc;
+    @Override
+    public Ingredient findOne(String id) {
+        return jdbc.queryForObject(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient, id);
+    }
+    private Ingredient mapRowToIngredient(ResultSet rs, int rowNum)
+            throws SQLException {
         return new Ingredient(
                 rs.getString("id"),
                 rs.getString("name"),
